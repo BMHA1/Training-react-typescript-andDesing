e7dff172d1627582f80a02f43b6e310b40a5c527
BHMA1 <miguelhanna05@gmail.com>
2022-09-10 18:15:13 +0200

    Defining structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import TicketList from './Containers/TicketList';
+
+function App() {
+  return (
+    <>
+        <TicketList/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Containers/TicketList.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+
+const TicketList = () =>  {
+
+    return (
+        <>
+            <h1>
+            </h1>
+        </>
+    )
+}
+
+export default TicketList;
