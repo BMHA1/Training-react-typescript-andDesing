27251aa0723cc97c5aefbb428b7cf430e6cad865
BHMA1 <miguelhanna05@gmail.com>
2022-09-12 14:18:11 +0200

    inserting the button structure into the sider

--- src/Containers/TicketList.tsx
+++ src/Containers/TicketList.tsx
@@ -1,30 +1,64 @@
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import TableListTicket from '../Components/TableListTicket/TableListTicket';
-import MenuHeaderCenter from '../Components/MenuHeaderCenter/MenuHeaderCenter';
 import { Layout } from 'antd';
 import React from 'react';
 import './TicketList.css'
-const { Sider, Content } = Layout;
+import vectorLogo from '../img/vectorLogo.svg';
+import lineas3 from '../img/menu/lineas3.svg';
+import apps from '../img/menu/apps.svg';
+import graduationcap from '../img/menu/graduationcap.svg';
+import stats from '../img/menu/stats.svg';
+import hojaLupa from '../img/menu/hojaLupa.svg';
+import interrogation from '../img/menu/interrogation.svg';
+import money from '../img/menu/money.svg';
 
-const TicketList = () =>  {
-    
-    let data =[1,2,3,4];
+const { Sider } = Layout;
+
+const TicketList: React.FC = () =>  {
+
     return (
         <>
             <Layout>
-                <Sider width={70}>
-                    <MenuHeaderCenter imagen = {data}/>
+                <Sider width={70} className='content-sider'>
+                    <div className='content-menu'>
+                        <Button type="link" >
+                            <img src={lineas3} alt='tex2'></img>
+                        </Button>
+                        <Button type="link" >
+                            <img src={vectorLogo} alt='tex2'></img>
+                        </Button>
+                    </div>
+                    <div className='content-menu'>
+                        <Button type="link" >
+                            <img src={apps} alt='tex2'></img>
+                        </Button>
+                        <Button type="link" >
+                            <img src={graduationcap} alt='tex2'></img>
+                        </Button>
+                        <Button type="link" >
+                            <img src={hojaLupa} alt='tex2'></img>
+                        </Button>
+                        <Button type="link" >
+                            <img src={stats} alt='tex2'></img>
+                        </Button>
+                        <Button type="link" >
+                            <img src={money} alt='tex2'></img>
+                        </Button>
+                        <Button type="link" >
+                            <img src={interrogation} alt='tex2'></img>
+                        </Button>
+                    </div>
                 </Sider>
                 <div className='content-main'>
                     <div className='content-presentation'>
                         <h1>
-                            Bienvenido de nuevo, Rodrígo. 👋 
+                            Bienvenido de nuevo, Rodrígo. 👋
                         </h1>
                         <span>
                             Estas son las personas que han comprado entrada
                         </span>
                     </div>
-                    <div className='content-table'> 
+                    <div className='content-table'>
                         <TableListTicket/>
                     </div>
                 </div>
